d7534ba69e5a6d6f0093178097cc433e1ef873d8
jmrf1 <reyesfigueroaj@student.douglascollege.ca>
2023-02-02 11:24:37 -0800

    Second commit from local machine

--- app/src/main/java/com/example/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/githubdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Made another change locally
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
